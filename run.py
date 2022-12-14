db72df1bafa2d7de89d35d9efa1cdfeeb420e80d
fatimamahate <fatimamahate@yahoo.co.uk>
2022-12-14 14:17:46 +0000

    Refactor checks into one funtion, nested for loops to ensure the user data for the particular student is printed

--- run.py
+++ run.py
@@ -13,59 +13,40 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CRED)
 SHEET = GSPREAD_CLIENT.open('make_the_grade')
 
 
-
-
-
-def school_data():
+def open_correct_sheet(sheet_name,field_name,input_question):
     """
-    The school_data function checks if the School ID inputted by user is valid
+    The open_correct_sheet function opens the relevant sheet and returns the user input
     """
-    
-    schools = SHEET.worksheet('school_number')
-    current_sheet=schools.get_all_records()
+    correct_sheet = SHEET.worksheet(sheet_name)
+    sheet_info = correct_sheet.get_all_records()
+    str_values = [str(record[field_name]) for record in sheet_info]
     check = False
     while not check:
-        user_school_no = input('What is your School ID? \n').strip()
-        for p in current_sheet:
-            if str(p['Number:']) == user_school_no:
-                current_school = SHEET.worksheet((f"{user_school_no}"))
-                print(current_school)
+        user_input = input(input_question).strip()
+        for info in str_values:
+            if info != user_input:
+                print('This number is invalid, try again')
+                break
+            else: 
+                for school in sheet_info:
+                    for value in school.values():
+                        if str(value) == user_input:
+                            print(school)
+                print('Thank you for the correct ID')
                 check = True
-                return user_school_no
-        print('Invalid school ID , try again or contact school admin \n')
+                break
+    return user_input
+
+def assessment_check(student):
+    """
     
-
-def user_data(id):
     """
-    The user_data function opens the correct school worksheet and asks user to input user ID.
-    It will check if the user id is in the data
-    """
-    school_id = str(id)
-    check = False
-    while not check:
-        user_user_no = input('What is your User ID? \n').strip()
-        correct_school = SHEET.worksheet(f'{id}')
-        current_sheet = correct_school.get_all_records()
-        # if not isdigit(user_user_no):
-        #     print('enter a valid number')
-        #     continue
-        for p in current_sheet:
-            if str(p['user number']) == user_user_no:
-                current_student = p
-                print(current_student)
-                check = True
-                return user_user_no
-        print('Invalid user ID , try again or contact school admin \n')
-
-# # def assessment_check(student):
-# #     """
-# #     The input in this will be the return of the previous function (user_user_no) which is a list and will ask user to fill in 
-# #     """
-# #     if student
+    
 
     
 
-act_school= school_data()
-student=user_data(act_school)
+
+user_school_input = open_correct_sheet('school_number', 'Number:', 'What is your School ID? \n')
+user_user_input = open_correct_sheet(user_school_input, 'user number', 'What is your User ID? \n')
 
 
