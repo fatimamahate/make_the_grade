b96018c00d0e65ed14de3034cc139ef6649c8ff9
fatimamahate <fatimamahate@yahoo.co.uk>
2022-12-14 19:06:16 +0000

    found bug in open_correct_sheet function - add assessment check function

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ def open_correct_sheet(sheet_name,field_name,input_question):
     correct_sheet = SHEET.worksheet(sheet_name)
     sheet_info = correct_sheet.get_all_records()
     str_values = [str(record[field_name]) for record in sheet_info]
+    print(str_values)
     check = False
     while not check:
         user_input = input(input_question).strip()
@@ -37,16 +38,42 @@ def open_correct_sheet(sheet_name,field_name,input_question):
                 break
     return user_input
 
-def assessment_check(student):
+def assessment_check(student,sheet_name,field_name):
     """
-    
+    This takes school number and user number from above
+    Checks if user has a target value and total number of assessments
     """
+    correct_sheet = SHEET.worksheet(sheet_name)
+    sheet_info = correct_sheet.get_all_records()
+    for school in sheet_info:
+        for value in school.values():
+            if str(value) == student:
+                print(school)
+    last_key = list(school)[-1]
+    if last_key == 'target':
+        check = False
+        while not check:
+            user_target_input=input('What is your target % for the end of the year (Enter a percentage)? \n')
+            if int(user_target_input) <= 0:
+                print('Minimum target is 1% try again')
+                continue
+            elif int(user_target_input) > 100:
+                print('Target cannot be larger than 100% try again')
+                continue
+            else:
+                print('Updating target...\n')
+                print(f'You need to achieve at least {last_key}% on each assessment')
+                check = True
+                break
     
-
+    user_assessment_no_input= input('What is the assessment number?') 
+    if user_assessment_no_input != int(last_key)+1:
+        print(f'Are you sure? The last assessment number was {last_key}')              
     
 
 
 user_school_input = open_correct_sheet('school_number', 'Number:', 'What is your School ID? \n')
 user_user_input = open_correct_sheet(user_school_input, 'user number', 'What is your User ID? \n')
+check_student_info = assessment_check(user_user_input, user_school_input, 'user number')
 
 
